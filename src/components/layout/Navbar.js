73183a981503d199ed26386ad91b9219e2125093
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-12 17:58:06 +0200

    - added logo for desktop navigation - Navbar component styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Logo from './Logo.svg';
+
 const Header = () => {
   return (
     <nav className='navbar'>
+      <img src={Logo} className='hide-mobile' />
       <ul>
-        <li>
+        <li className='active'>
           <Link to='/'>Home</Link>
         </li>
         <li>
