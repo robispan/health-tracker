8967965be5b43a5717eed7f49af638b2e084236d
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-12 15:46:51 +0200

    first upload - basic setup with react router

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Dashboard from './components/pages/Dashboard';
+
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav className='navbar'>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/dashboard'>Dashboard</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
