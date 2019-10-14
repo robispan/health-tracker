71b04685c6c47783491af6b0d8535dc903fbdd81
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-14 21:09:39 +0200

    added inner navbar div

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,19 +6,21 @@ import Logo from './Logo.svg';
 const Header = () => {
   return (
     <nav className='navbar'>
-      <img src={Logo} className='hide-mobile' />
-      <ul>
-        <li>
-          <NavLink exact={true} activeClassName='active' to='/'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/dashboard' activeClassName='active'>
-            Dashboard
-          </NavLink>
-        </li>
-      </ul>
+      <div className='navbar-inner'>
+        <img src={Logo} className='hide-mobile' />
+        <ul>
+          <li>
+            <NavLink exact={true} activeClassName='active' to='/'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/dashboard' activeClassName='active'>
+              Dashboard
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
