ea213a3f91aaa7f458c015f326aa954e3e12c7c4
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-14 21:55:12 +0200

    imported MetricsCard component to Dashboard

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import MetricsCard from './MetricsCard';
+
 const Dashboard = () => {
   return (
     <div>
       <h2 className='center upper'>Dashboard</h2>
+      <div className='grid'>
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+        <MetricsCard />
+      </div>
     </div>
   );
 };
