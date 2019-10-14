5ddeea7d2c65bf44e20b6f020002541f0b429052
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-14 20:38:05 +0200

    added service providers cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import Card from '../assets/Card';
+import StravaImg from '../assets/images/strava.jpg';
+import NutriticsImg from '../assets/images/nutritics.png';
+import RunkeeperImg from '../assets/images/runkeeper.jpg';
+import TomtomImg from '../assets/images/tomtom.png';
+import LifelogImg from '../assets/images/lifelog.png';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h2 className='center upper'>Connect your apps</h2>
+
+      <div className='card-container'>
+        <Card imageUrl={StravaImg} />
+        <Card imageUrl={NutriticsImg} />
+        <Card imageUrl={RunkeeperImg} />
+        <Card imageUrl={TomtomImg} />
+        <Card imageUrl={LifelogImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
