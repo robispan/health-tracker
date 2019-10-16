9cf915e8ba8c0b405c680d4247e79591b0e3290e
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-16 21:58:43 +0200

    - fetch auth tokens on StravaRedirect page instead of in context state - clean up unnecessary code in stravaState

--- src/components/pages/StravaRedirect.js
+++ src/components/pages/StravaRedirect.js
@@ -1,6 +1,10 @@
 import { useContext, useEffect } from 'react';
+import axios from 'axios';
 
 import StravaContext from '../../context/strava/stravaContext';
+
+const stravaClientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
+const stravaClientSecret = process.env.REACT_APP_STRAVA_CLIENT_SECRET;
 
 const StravaRedirect = ({ location, history }) => {
   const { getStravaData } = useContext(StravaContext);
@@ -10,11 +14,23 @@ const StravaRedirect = ({ location, history }) => {
     const params = new URLSearchParams(location.search);
     const authCode = params.get('code');
 
-    // Call action to get athlete data
-    getStravaData(authCode);
+    fetchData();
 
-    // Redirect to homepage
-    history.push('/');
+    async function fetchData() {
+      // Get auth tokens
+      const res = await axios.post(
+        `https://www.strava.com/oauth/token?client_id=${stravaClientId}&client_secret=${stravaClientSecret}&code=${authCode}&grant_type=authorization_code`
+      );
+
+      // Call action to get athlete data
+      getStravaData(res.data.access_token);
+
+      // Save refresh token to sessionStorage
+      sessionStorage.setItem('_strava_rt', res.data.refresh_token);
+
+      // Redirect to homepage
+      history.push('/');
+    }
   });
 
   return null;
--- src/context/strava/StravaState.js
+++ src/context/strava/StravaState.js
@@ -4,13 +4,8 @@ import StravaReducer from './stravaReducer';
 import StravaContext from './stravaContext';
 import { GET_STRAVA_DATA, SET_LOADING } from '../types';
 
-let stravaClientId;
-let stravaClientSecret;
-let baseUrl;
-
-stravaClientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
-stravaClientSecret = process.env.REACT_APP_STRAVA_CLIENT_SECRET;
-baseUrl = process.env.REACT_APP_BASE_URL;
+const stravaClientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 const StravaState = props => {
   const initialState = {
@@ -27,24 +22,14 @@ const StravaState = props => {
   };
 
   // Get athlete data
-  const getStravaData = async code => {
+  const getStravaData = async access_token => {
     setLoading();
 
-    const authRes = await axios.post(
-      `https://www.strava.com/oauth/token?client_id=${stravaClientId}&client_secret=${stravaClientSecret}&code=${code}&grant_type=authorization_code`
-    );
-
-    // Save refresh token to sessionStorage
-    sessionStorage.setItem('_strava_rt', authRes.data.refresh_token);
-
-    // Get detailed data
     const dataRes = await axios.get(`https://www.strava.com/api/v3/athlete`, {
       headers: {
-        Authorization: `Bearer ${authRes.data.access_token}`
+        Authorization: `Bearer ${access_token}`
       }
     });
-
-    console.log('strava data sent to state', dataRes.data);
 
     dispatch({
       type: GET_STRAVA_DATA,
