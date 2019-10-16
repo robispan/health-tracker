a87b305670cf2086e71ea6051afcac4a735ead7b
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-16 20:25:34 +0200

    add strava redirect page to routes - wrap with strava context state

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home/Home';
 import Dashboard from './components/pages/Dashboard/Dashboard';
+import StravaRedirect from './components/pages/StravaRedirect';
+
+import StravaState from './context/strava/StravaState';
 
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <div className='container'>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/dashboard'>
-            <Dashboard />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <StravaState>
+      <Router>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/dashboard'>
+              <Dashboard />
+            </Route>
+
+            {/* Tracker apps redirects */}
+            <Route exact path='/strava-auth' component={StravaRedirect} />
+          </Switch>
+        </div>
+      </Router>
+    </StravaState>
   );
 }
 
