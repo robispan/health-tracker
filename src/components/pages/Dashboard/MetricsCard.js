55b715a3f18235df4bb571fb071491e071d8b1de
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-16 20:56:53 +0200

    display metrics from props - added not connected text if no apps are connected on homepage

--- src/components/pages/Dashboard/MetricsCard.js
+++ src/components/pages/Dashboard/MetricsCard.js
@@ -3,24 +3,28 @@ import React from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
-const MetricsCard = () => {
-  const percentage = 66;
-
+const MetricsCard = ({ ratio, label }) => {
   return (
     <div className='metrics-card'>
-      <h3 className='metrics-card-title'>Title</h3>
-      <CircularProgressbar
-        value={percentage}
-        text={`${percentage}%`}
-        background
-        backgroundPadding={6}
-        styles={buildStyles({
-          backgroundColor: '#00b9bb',
-          textColor: '#fff',
-          pathColor: '#fff',
-          trailColor: 'transparent'
-        })}
-      />
+      <h3 className='metrics-card-title center'>{label}</h3>
+      {ratio ? (
+        <CircularProgressbar
+          value={ratio}
+          text={`${ratio}%`}
+          background
+          backgroundPadding={6}
+          styles={buildStyles({
+            backgroundColor: '#00b9bb',
+            textColor: '#fff',
+            pathColor: '#fff',
+            trailColor: 'transparent'
+          })}
+        />
+      ) : (
+        <div className='center'>
+          Connect trackers on homepage to see your progression!
+        </div>
+      )}
     </div>
   );
 };
