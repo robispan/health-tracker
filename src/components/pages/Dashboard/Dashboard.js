c080c3ac5b72726de623a64acedd305241183fe6
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-16 20:55:48 +0200

    get strava athlete data from context - set sample target metrics - calculate weight ratio - send weight metric and label to MetricsCard via props

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import StravaContext from '../../../context/strava/stravaContext';
 
 import MetricsCard from './MetricsCard';
 
 const Dashboard = () => {
+  const { stravaData } = useContext(StravaContext);
+
+  const targetMetrics = {
+    targetWeight: 50,
+    heartRate: 55
+  };
+
+  const weightRatio =
+    stravaData && stravaData.weight
+      ? Math.round((100 * targetMetrics.targetWeight) / stravaData.weight)
+      : null;
+
   return (
     <div>
       <h2 className='center upper'>Dashboard</h2>
       <div className='grid'>
-        <MetricsCard />
-        <MetricsCard />
-        <MetricsCard />
-        <MetricsCard />
+        {/* Weight */}
+        <MetricsCard label='Weight loss' ratio={weightRatio} />
+
+        {/* Heart rate */}
+        <MetricsCard label='Heart rate' />
       </div>
     </div>
   );
