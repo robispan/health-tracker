ee88462a5c19a979e08db8197e8f04500df9fc3a
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-21 20:06:51 +0200

    added test for App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home/Home';
@@ -8,8 +9,11 @@ import StravaRedirect from './components/pages/StravaRedirect';
 import Page404 from './components/pages/Page404';
 
 import StravaState from './context/strava/StravaState';
-
 import './App.css';
+
+const LocationDisplay = withRouter(({ location }) => (
+  <div data-testid='location-display'>{location.pathname}</div>
+));
 
 function App() {
   return (
@@ -30,6 +34,8 @@ function App() {
             {/* 404 */}
             <Route component={Page404} />
           </Switch>
+
+          <LocationDisplay />
         </div>
       </Router>
     </StravaState>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('full app rendering/navigating', () => {
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  // verify page content for expected route
+  expect(getByText('Connect your apps')).toBeInTheDocument();
+
+  fireEvent.click(getByText(/Dashboard/i));
+
+  // check that the content changed to the new page
+  expect(getByText('Weight loss progression')).toBeInTheDocument();
 });
