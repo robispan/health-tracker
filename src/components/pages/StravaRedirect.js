5597794ec1c334b075413ef0dbf6037ef1b94ee0
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-21 18:44:59 +0200

    updated comment

--- src/components/pages/StravaRedirect.js
+++ src/components/pages/StravaRedirect.js
@@ -15,14 +15,13 @@ const StravaRedirect = ({ location, history }) => {
     const authCode = params.get('code');
 
     fetchData();
-
     async function fetchData() {
       // Get auth tokens
       const res = await axios.post(
         `https://www.strava.com/oauth/token?client_id=${stravaClientId}&client_secret=${stravaClientSecret}&code=${authCode}&grant_type=authorization_code`
       );
 
-      // Call action to get athlete data
+      // Call context action to collect athlete data
       getStravaData(res.data.access_token);
 
       // Save refresh token to sessionStorage
