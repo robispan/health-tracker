904cf3e3e8e82c73ed35fba07fe0c3da88bc2608
DESKTOP-7PU74MV\Robi <robi.span@gmail.com>
2019-10-22 00:07:49 +0200

    added calories burn metric from runkeeper API

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,20 +1,30 @@
 import React, { useContext } from 'react';
 
 import StravaContext from '../../../context/strava/stravaContext';
+import RunKeeperContext from '../../../context/runKeeper/runKeeperContext';
 
 import MetricsCard from './MetricsCard';
 
 const Dashboard = () => {
   const { stravaData } = useContext(StravaContext);
+  const { runKeeperData } = useContext(RunKeeperContext);
 
   const targetMetrics = {
     targetWeight: 50,
-    heartRate: 55
+    targetCaloriesLost: 800
   };
 
   const weightRatio =
     stravaData && stravaData.weight
       ? Math.round((100 * targetMetrics.targetWeight) / stravaData.weight)
+      : null;
+
+  const caloriesRatio =
+    runKeeperData && runKeeperData.activities
+      ? Math.round(
+          (100 * runKeeperData.activities[1].calories) /
+            targetMetrics.targetCaloriesLost
+        )
       : null;
 
   return (
@@ -24,8 +34,8 @@ const Dashboard = () => {
         {/* Weight */}
         <MetricsCard label='Weight loss progression' ratio={weightRatio} />
 
-        {/* Heart rate */}
-        <MetricsCard label='Heart rate progression' />
+        {/* Calories */}
+        <MetricsCard label='Calories burn progression' ratio={caloriesRatio} />
       </div>
     </div>
   );
