fae2c4156abeea79cb9fe16353987f13fbe79c0e
Robert Span <robi.span@gmail.com>
2020-02-03 19:27:24 +0100

    updated packages, changed logo

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import Logo from './Logo.svg';
+import Logo from './Logo.png';
 
 const Header = () => {
-  return (
-    <nav className='navbar'>
-      <div className='navbar-inner'>
-        <NavLink to='/' className='navbar-logo'>
-          <img src={Logo} className='hide-mobile' alt='logo' />
-        </NavLink>
-        <ul>
-          <li>
-            <NavLink exact={true} activeClassName='active' to='/'>
-              Home
+	return (
+		<nav className='navbar'>
+			<div className='navbar-inner'>
+				<NavLink to='/' className='navbar-logo'>
+					<img src={Logo} className='hide-mobile' alt='logo' />
+				</NavLink>
+				<ul>
+					<li>
+						<NavLink exact={true} activeClassName='active' to='/'>
+							Home
             </NavLink>
-          </li>
-          <li>
-            <NavLink to='/dashboard' activeClassName='active'>
-              Dashboard
+					</li>
+					<li>
+						<NavLink to='/dashboard' activeClassName='active'>
+							Dashboard
             </NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
