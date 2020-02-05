5ab5800c863c9680c1afc4a0796512bc3f9d7c34
Robert Span <robi.span@gmail.com>
2020-02-05 13:16:07 +0100

    use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 
 import LoadingContext from './context/loading/loadingContext';
