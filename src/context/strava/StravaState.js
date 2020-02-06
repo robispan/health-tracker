43070415f6ea4b3c2c511574eadc4096c4b97378
Robert Span <robi.span@gmail.com>
2020-02-06 22:46:18 +0100

    test

--- src/context/strava/StravaState.js
+++ src/context/strava/StravaState.js
@@ -9,63 +9,63 @@ const stravaClientId = process.env.REACT_APP_STRAVA_CLIENT_ID;
 const baseUrl = process.env.REACT_APP_BASE_URL;
 
 const StravaState = props => {
-  const { setLoading, stopLoading } = useContext(LoadingContext);
+	const { setLoading, stopLoading } = useContext(LoadingContext);
 
-  const initialState = {
-    stravaData: null
-  };
+	const initialState = {
+		stravaData: null
+	};
 
-  const [state, dispatch] = useReducer(StravaReducer, initialState);
+	const [state, dispatch] = useReducer(StravaReducer, initialState);
 
-  // Authorize app to make API calls for strava user
-  // User is redirected to /strava-auth after authorization
-  const authStrava = () => {
-    window.location.href = `https://www.strava.com/oauth/authorize?client_id=${stravaClientId}&redirect_uri=${baseUrl}%2F%23%2Fstrava-auth/&response_type=code&scope=profile:read_all`;
-  };
+	// Authorize app to make API calls for strava user
+	// User is redirected to /strava-auth after authorization
+	const authStrava = () => {
+		window.location.href = `https://www.strava.com/oauth/authorize?client_id=${stravaClientId}&redirect_uri=${baseUrl}%2F%23%2Fhealth-tracker/strava-auth&response_type=code&scope=profile:read_all`;
+	};
 
-  // Get athlete data
-  const getStravaData = async access_token => {
-    setLoading();
+	// Get athlete data
+	const getStravaData = async access_token => {
+		setLoading();
 
-    try {
-      const dataRes = await axios.get(`https://www.strava.com/api/v3/athlete`, {
-        headers: {
-          Authorization: `Bearer ${access_token}`
-        }
-      });
+		try {
+			const dataRes = await axios.get(`https://www.strava.com/api/v3/athlete`, {
+				headers: {
+					Authorization: `Bearer ${access_token}`
+				}
+			});
 
-      dispatch({
-        type: GET_STRAVA_DATA,
-        payload: dataRes.data
-      });
-    } catch (error) {
-      console.log('error fetching Strava athlete data', error);
-    }
+			dispatch({
+				type: GET_STRAVA_DATA,
+				payload: dataRes.data
+			});
+		} catch (error) {
+			console.log('error fetching Strava athlete data', error);
+		}
 
-    stopLoading();
-  };
+		stopLoading();
+	};
 
-  // Clear strava data
-  const clearStravaData = () => {
-    sessionStorage.removeItem('_strava_rt');
+	// Clear strava data
+	const clearStravaData = () => {
+		sessionStorage.removeItem('_strava_rt');
 
-    dispatch({
-      type: CLEAR_STRAVA_DATA
-    });
-  };
+		dispatch({
+			type: CLEAR_STRAVA_DATA
+		});
+	};
 
-  return (
-    <StravaContext.Provider
-      value={{
-        stravaData: state.stravaData,
-        authStrava,
-        getStravaData,
-        clearStravaData
-      }}
-    >
-      {props.children}
-    </StravaContext.Provider>
-  );
+	return (
+		<StravaContext.Provider
+			value={{
+				stravaData: state.stravaData,
+				authStrava,
+				getStravaData,
+				clearStravaData
+			}}
+		>
+			{props.children}
+		</StravaContext.Provider>
+	);
 };
 
 export default StravaState;
