fe323c2d3e3a40d93ff0b942283e431d7f32db86
Robert Span <robi.span@gmail.com>
2020-02-06 22:56:55 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 
 import LoadingContext from './context/loading/loadingContext';
